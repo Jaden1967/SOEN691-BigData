2d04c000dc8e9b2f0871a4eb1b8a2a3ffb18ff61
Xiyun Zhang <zhxyun201709@163.com>
2020-03-29 00:38:04 -0400

    getDiffDays

--- preprocesser.py
+++ preprocesser.py
@@ -1,5 +1,6 @@
 
 from pyspark.sql import *
+from datetime import date
 
 
 def init_spark():
@@ -9,6 +10,16 @@ def init_spark():
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
     return spark
+def getDiffDays(startDate, endDate):
+    print(startDate)
+    startDateList = map(lambda x : int(x), startDate.split('-'))
+    endDateList = map(lambda x: int(x), endDate.split('-'))
+    startDate = date(*startDateList)
+    endDate = date(*endDateList)
+    diffDays = (endDate - startDate).days
+    print('diff days: ' + str(diffDays))
+    return diffDays
+
 originalDataPath = './data/steam.csv'
 spark = init_spark()
 rawData = spark.read.csv(originalDataPath, header=True)
@@ -17,4 +28,7 @@ print(rawData.columns)
 #remove unwanted columns
 for it in ['appid', 'name', 'publisher', 'genre']:
     rawData = rawData.drop(it)
-print(rawData.columns)
+# rawData.printSchema()
+#convert release date to diff days since release to 2019-05-15
+rawData.select('release_date').rdd.flatMap(lambda x : (getDiffDays(x.release_date, '2019-05-15'), )).toDF()#这块我不会写，就是把release_date所有row换成diffdays
+rawData.show()
