e590a163d16597f2a540dfe24e638bf4304a9806
Xiyun Zhang <zhxyun201709@163.com>
2020-03-29 05:03:40 -0400

    changed to udf, added positive rating ratio, number of owners

--- preprocesser.py
+++ preprocesser.py
@@ -1,7 +1,8 @@
 
 from pyspark.sql import *
-from pyspark.sql.functions import lit
+from pyspark.sql.functions import udf
 from datetime import date
+from pyspark.sql.types import StringType
 
 
 def init_spark():
@@ -11,27 +12,59 @@ def init_spark():
         .config("spark.some.config.option", "some-value") \
         .getOrCreate()
     return spark
-def getDiffDays(startDate, endDate):
+def getDiffDays(startDate):#get diff days since release to 2019-05-15
     
     startDateList = map(lambda x : int(x), startDate.split('-'))
-    endDateList = map(lambda x: int(x), endDate.split('-'))
+    endDateList = [2019, 5, 15]
     startDate = date(*startDateList)
     endDate = date(*endDateList)
     diffDays = (endDate - startDate).days
     return str(diffDays)
-    
-    
+
+def getPositiveRatingRatio(po, ne):
+    return str(int(po)/(int(po) + int(ne)))
+def getNumberOfOwners(owners):
+    # try:
+    #     li = map(lambda x : int(x), owners.split('-'))
+    #     return str(int(sum(li)/2))
+    # except ValueError:
+    #     print('caught' + owners)
+    #     exit(1)
+    li = map(lambda x : int(x), owners.split('-'))
+    return str(int(sum(li)/2))
     
 originalDataPath = './data/steam.csv'
-unwantedCols = ['appid', 'name', 'publisher', 'genres']
+unwantedCols1 = ['appid', 'name', 'publisher', 'genres']
 spark = init_spark()
-rawData = spark.read.option("quote", "\"").option("escape", "\"").csv(originalDataPath, header=True)
+df = spark.read.option("quote", "\"").option("escape", "\"").csv(originalDataPath, header=True)
 
 #remove unwanted columns
-for it in unwantedCols:
-    rawData = rawData.drop(it)
-rdd = rawData.rdd
-#convert release date to diff days since release to 2019-05-15
-df = rdd.map(lambda x : x + (getDiffDays(x.release_date, '2019-05-15'),)).toDF(rawData.columns + ['days'])
+for it in unwantedCols1:
+    df = df.drop(it)
+#add days column
+udfGetDiffDays = udf(getDiffDays, StringType())
+df = df.withColumn('days', udfGetDiffDays(df.release_date))
 df = df.drop('release_date')
 df.show()
+#add positive rating ratio column
+# df = rdd.map(lambda x : x + (getPositiveRatingRatio(x.positive_ratings, x.negative_ratings),)).toDF(rawData.columns + ['positive_rating_ratio'])
+udfGetPositiveRatingRatio = udf(getPositiveRatingRatio, StringType())
+df = df.withColumn('positive_rating_ratio', udfGetPositiveRatingRatio(df.positive_ratings, df.negative_ratings))
+df = df.drop('positive_ratings')
+df = df.drop('negative_ratings')
+df.show()
+
+#add number_of_owners column
+# rdd = df.rdd
+# df = rdd.map(lambda x : x + (getNumberOfOwners(x.owners),)).toDF(rawData.columns + ['number_of_owners'])
+udfGetNumberOfOwners = udf(getNumberOfOwners, StringType())
+df = df.withColumn('number_of_owners', udfGetNumberOfOwners(df.owners))
+df = df.drop('owners')
+df.show()
+
+
+
+
+
+
+
