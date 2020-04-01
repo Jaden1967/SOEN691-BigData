8654f0528e7e97448bb731538c109f9bb849a7b7
Xiyun Zhang <zhxyun201709@163.com>
2020-03-31 23:34:35 -0400

    linear regression

--- predictor.py
+++ predictor.py
@@ -1,10 +1,31 @@
 from preprocesser import generate_dataset
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import RegressionEvaluator
 
-def reFormat(df):
-    df = df.drop(df.columns.difference(['features', 'positive_rating_ratio']), 1, inplace=True)
-    df = df.withColumnRenamed('positive_rating_ratio', 'label')
-    return df
+def predict(algorithm, paramGrid):
+    pipeline = Pipeline(stages=[algorithm])
+    crossval = CrossValidator(estimator=pipeline,
+                            estimatorParamMaps=paramGrid,
+                            evaluator=RegressionEvaluator(),
+                            numFolds=5)#5 folds
+    # Run cross-validation, and choose the best set of parameters.
+    model = crossval.fit(training)
+    print("Coefficients: " + str(model.coefficients))
+
+    #make prediction
+    predictions =model.transform(testing)
+    #print prediction samples
+    predictions.select("features", "positive_rating_ratio", "prediction").show(5)
+    #evaluate rmse
+    evaluator = RegressionEvaluator(predictionCol="prediction", \
+                    labelCol="positive_rating_ratio",metricName="rmse")
+    print("rmse on test data = %g" % evaluator.evaluate(predictions))
 
 training, testing = generate_dataset()
-training = reFormat(training)
-testing = reFormat(testing)
+
+#linear regression
+lr = LogisticRegression(featuresCol='features', lableCol='positive_rating_ratio', maxIter=10)
+paramGrid = ParamGridBuilder().addGrid(lr.regParam, [0.5, 0.3, 0.1, 0.05, 0.01]).build()
+predict(lr, paramGrid)
+
